7986d1d6fc4a9c80e3adb28295ed3f4820587410
ayhan219 <ayhan-games@hotmail.com>
2024-10-02 14:01:21 +0300

    authMiddleware added

--- api/routes/UserRoute.js
+++ api/routes/UserRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {signupUser,loginUser} = require("../controller/UserController")
+const {signupUser,loginUser,getCurrentUser} = require("../controller/UserController")
+const AuthMiddleware = require("../middleware/AuthMiddleware")
 
 
 router.post("/signup",signupUser);
 router.post("/login",loginUser)
+router.get("/getcurrent",AuthMiddleware,getCurrentUser)
 
 
 module.exports = router;
