7576f561d85e77235ecfbdef8f61550f85a4bf01
ayhan219 <ayhan-games@hotmail.com>
2024-10-11 12:35:02 +0300

    shoppingcart page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./pages/Signup"
 import Products from "./pages/Products"
 import Login from "./pages/Login"
 import { UserProvider } from "./UserContext/UserContext"
+import ShoppingCart from "./pages/ShoppingCart"
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/shoppingcart" element={<ShoppingCart />} />
       </Routes>
       <Footer />
       </UserProvider>
---
+++ client/src/pages/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <div className='w-full h-screen'>
+        <div className='w-[80%] h-full bg-black'>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default ShoppingCart
