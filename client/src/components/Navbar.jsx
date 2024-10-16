c480c8d61b039ace7e71140b5256077efff26698
ayhan219 <ayhan-games@hotmail.com>
2024-10-16 14:22:27 +0300

    navbar updated for favorite link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -146,9 +146,11 @@ const Navbar = () => {
         <div className="flex items-center gap-3 text-lg cursor-pointer  relative  after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full">
           <MdFavoriteBorder className="text-2xl md:text-3xl " />
           <Link to={"/favorites"}>
+          <Link to={"/favorites"}>
           <a href="" className="hidden md:block">
             Favorite
           </a>
+          </Link>
           </Link>
         </div>
         <div className="flex items-center gap-3 text-lg  cursor-pointer  relative  after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full">
