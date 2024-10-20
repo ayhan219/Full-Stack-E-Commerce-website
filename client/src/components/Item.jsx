6bc981df1aab349e1b3f70e395b400e884d0936c
ayhan219 <ayhan-games@hotmail.com>
2024-10-20 20:53:23 +0300

    productroute updated

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -3,32 +3,31 @@ import { IoIosStar } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { UserContext } from "../UserContext/UserContext";
 
-const Item = ({item,key}) => {
+const Item = ({ item, key }) => {
   const images = item.image.split(",");
-  const {productId,setProductId} = useContext(UserContext);
+  const { productId, setProductId } = useContext(UserContext);
 
-  
   return (
-    <div key={key} className="w-96 h-[32rem] bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 duration-150">
-      
+    <div
+      key={key}
+      className="w-96 h-[32rem] bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 duration-150"
+    >
       <div className="w-full h-[65%] cursor-pointer">
-       <Link to={`/products/${item.id}`}>
-       <img 
-       onClick={()=>setProductId(item.id)}
-          className="object-cover w-full h-full"
-          src={`http://localhost:5000/${images[0]}`}
-          alt="T-Shirt"
-        />
-       </Link>
+        <Link to={`/products/${item.id}`}>
+          <img
+            onClick={() => setProductId(item.id)}
+            className="object-contain w-full h-full"
+            src={`http://localhost:5000/${images[0]}`}
+            alt="T-Shirt"
+          />
+        </Link>
       </div>
-      
-      
+
       <div className="p-4 text-[#555555] text-lg flex flex-col gap-2">
         <h3 className="font-semibold">{item.type}</h3>
         <p className="text-sm text-gray-500">{item.description}</p>
       </div>
 
-      
       <div className="flex items-center px-4 text-2xl">
         <IoIosStar className="text-yellow-400" />
         <IoIosStar className="text-yellow-400" />
@@ -38,7 +37,6 @@ const Item = ({item,key}) => {
         <span className="pl-3 text-base text-gray-600">(8)</span>
       </div>
 
-    
       <div className="flex items-center px-4 pt-4 text-3xl font-bold">
         <span>$</span>
         <p className="ml-1">{item.price}</p>
